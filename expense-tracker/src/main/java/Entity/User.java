37fa0aebe9eae1bde8c883f546333fe1c67baa31
susmitha <thallasusmitha548@gmail.com>
2026-06-28 17:09:30 +0530

    Created Repository Package and added  the UserRepository Interface

---
+++ expense-tracker/src/main/java/Entity/User.java
@@ -0,0 +1,57 @@
+package Entity;
+
+import jakarta.persistence.*;
+@Entity @Table(name = "users")
+public class User {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    @Column(unique = true,nullable = false)
+    private  String email;
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Column(unique = true,nullable = false)
+    private String username;
+
+
+}
